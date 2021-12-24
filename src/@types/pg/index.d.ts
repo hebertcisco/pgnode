14e3310445cda707b3e1b17f01a1127266b6ed17
hebertcisco <hebertcisco@outlook.com>
2021-12-24 12:52:04 -0300

    v1.0.5

--- src/@types/pg/index.d.ts
+++ src/@types/pg/index.d.ts
@@ -1,13 +1,13 @@
 import { Client, ClientBase, Connection, Events, Pool, Query, native, types } from 'pg';
 export type PgType = {
-    Client: typeof Client;
-    ClientBase: typeof ClientBase;
-    Connection: typeof Connection;
-    Events: typeof Events;
-    Pool: typeof Pool;
-    Query: typeof Query;
-    native: typeof native;
-    types: typeof types;
+  Client: typeof Client;
+  ClientBase: typeof ClientBase;
+  Connection: typeof Connection;
+  Events: typeof Events;
+  Pool: typeof Pool;
+  Query: typeof Query;
+  native: typeof native;
+  types: typeof types;
 };
 
 export type UnknownType = unknown | undefined | null;
--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,11 +1,11 @@
 import pg from '../index';
 
 const config = {
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: Number(process.env.POSTGRES_PORT)
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: Number(process.env.POSTGRES_PORT),
 };
 const client = new pg.Client({ ...config });
 
