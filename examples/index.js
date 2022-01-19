2ce25d57b46ac31f8e1909711aa94413840c82c8
Restyled.io <commits@restyled.io>
2022-01-19 01:53:16 +0000

    Restyled by prettier

--- examples/index.js
+++ examples/index.js
@@ -1,36 +1,36 @@
-import {createServer} from 'http';
+import { createServer } from 'http';
 
-import {createTable, createTableGenerator} from './tx.js';
+import { createTable, createTableGenerator } from './tx.js';
 
 (async () => {
   const server = createServer(async (req, res) => {
     if (req.method === 'POST' && req.url === '/createTableGenerator') {
       await createTableGenerator()
-          .next()
-          .then((returns) => {
-            res.writeHead(200, {'Content-Type' : 'text/plain'});
-            res.end(`${returns.value}`);
-          })
-          .catch((err) => {
-            res.writeHead(500, {'Content-Type' : 'text/plain'});
-            res.end(`${JSON.stringify(err)}`);
-          });
+        .next()
+        .then((returns) => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`${returns.value}`);
+        })
+        .catch((err) => {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(`${JSON.stringify(err)}`);
+        });
     }
     if (req.method === 'POST' && req.url === '/createTable') {
       await createTable()
-          .then((returns) => {
-            res.writeHead(200, {'Content-Type' : 'text/plain'});
-            res.end(`${JSON.stringify(returns)}`);
-          })
-          .catch((err) => {
-            res.writeHead(500, {'Content-Type' : 'text/plain'});
-            res.end(`${JSON.stringify(err)}`);
-          });
+        .then((returns) => {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(`${JSON.stringify(returns)}`);
+        })
+        .catch((err) => {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end(`${JSON.stringify(err)}`);
+        });
     } else if (req.method === 'GET' && req.url === '/') {
-      res.writeHead(200, {'Content-Type' : 'text/plain'});
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('pgnode example');
     }
   });
   server.listen(3000);
   console.log(`Server running at http://localhost:3000/`);
-})()
+})();
